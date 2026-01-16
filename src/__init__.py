5ac02c3ac32dc71287b05209ec106d26dc528602
S1ii <ilya.sitnikov29@gmail.com>
2026-01-17 02:37:52 +0500

    feat: Initialize `src` package with metadata and expose `Client` and `ChatManager`.

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,6 @@ __version__ = "1.0.0"
 __author__ = "Nekto.me Monitor Contributors"
 
 from src.client import Client
-from src.chat import Chat
 from src.chat_manager import ChatManager
 
-__all__ = ['Client', 'Chat', 'ChatManager']
+__all__ = ['Client', 'ChatManager']
